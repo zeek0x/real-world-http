5af2954365c273dacd2fdeac2010c1575fa02da9
zeek0x <0h23k1.n40k1@gmail.com>
2020-08-27 11:34:29 +0900

    Arrange Layout.

--- src/simpleget/main.go
+++ src/simpleget/main.go
@@ -17,4 +17,8 @@ func main() {
 		panic(err)
 	}
 	log.Println(string(body))
+	log.Println("Status:", resp.Status)
+	log.Println("StatusCode:", resp.StatusCode)
+	log.Println("Headers:", resp.Header)
+	log.Println("Content-Length", resp.Header.Get("Content-Length"))
 }
