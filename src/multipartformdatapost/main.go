c44f734133c11ff6ec859f8c30acaae9e7d0a68d
zeek0x <0h23k1.n40k1@gmail.com>
2020-08-31 11:07:03 +0900

    Add multipartformdatapost.

---
+++ src/multipartformdatapost/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+func main() {
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	writer.WriteField("name", "Michael Jackson")
+
+	fileWriter, err := writer.CreateFormFile("thumbnail", "photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+
+	readFile, err := os.Open("photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer readFile.Close()
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buffer)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
+}
