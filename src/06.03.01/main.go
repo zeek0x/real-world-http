3cea2dd194228deff9a4512f68723cc14c7a5df8
zeek0x <0h23k1.n40k1@gmail.com>
2020-11-05 11:41:20 +0900

    Add 06.03.

---
+++ src/06.03.01/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"io"
+	"time"
+)
+
+func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
+	// このエンドポイントでは変更以外は受け付けない
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
+		w.WriteHeader(400)
+		return
+	}
+	fmt.Println("Upgrade to MyProtocol")
+
+	// 低層のソケットを取得
+	hijacker := w.(http.Hijacker)
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+		return
+	}
+	defer conn.Close()
+
+	// プロトコルが変わるというレスポンスを送信
+	response := http.Response{
+		StatusCode: 101,
+		Header: make(http.Header),
+	}
+	response.Header.Set("Upgrade", "MyProtocol")
+	response.Header.Set("Connection", "Upgrade")
+	response.Write(conn)
+
+	// オリジナルの通信の開始
+	for i := 0; i < 10; i++ {
+		fmt.Fprintf(readWriter, "%d\n", i)
+		fmt.Println("->", i)
+		readWriter.Flush() // Trigger "chunked" encoding and send a chunk...
+		recv, err := readWriter.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+		fmt.Printf("<- %s", string(recv))
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func main() {
+	var httpServer http.Server
+	http.HandleFunc("/upgrade", handlerUpgrade)
+	log.Println("start http listening :18888")
+	httpServer.Addr = ":18888"
+	log.Println(httpServer.ListenAndServe())
+}
