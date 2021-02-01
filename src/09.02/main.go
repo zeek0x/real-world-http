8203f54c290cd7cfe3e5d1ba124e2eb800ad0e00
zeek0x <0h23k1.n40k1@gmail.com>
2021-02-01 12:03:24 +0900

    Add 09.02.

---
+++ src/09.02/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var image []byte
+
+func init() {
+	var err error
+	image, err = ioutil.ReadFile("./image.png")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handlerHtml(w http.ResponseWriter, r *http.Request) {
+	pusher, ok := w.(http.Pusher)
+	if ok {
+		pusher.Push("/image", nil)
+	}
+	w.Header().Add("Content-Type", "text/html")
+	fmt.Fprintf(w, `<html><body><img src="/image"></body></html>`)
+}
+
+func handlerImage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(image)
+}
+
+func main() {
+	http.HandleFunc("/", handlerHtml)
+	http.HandleFunc("/image", handlerImage)
+	fmt.Println("start http listening :18443")
+	err := http.ListenAndServeTLS(":18443", "ssl/server.crt", "ssl/server.key", nil)
+	fmt.Println(err)
+}
